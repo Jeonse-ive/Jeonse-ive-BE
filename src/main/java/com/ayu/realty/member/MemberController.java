d2472b59a75eb9193dc841fb0bf7bfce706ce37f
goodjunseon <pzs2001926@gmail.com>
2025-05-20 21:19:07 +0900

    Feat: 회원가입(Sign_In) 로직 구현

---
+++ src/main/java/com/ayu/realty/member/MemberController.java
@@ -0,0 +1,45 @@
+package com.ayu.realty.member;
+
+import com.ayu.realty.global.dto.ApiRes;
+import com.ayu.realty.global.response.ErrorType.ErrorCode;
+import com.ayu.realty.global.response.SuccessType.MemberSuccessCode;
+import com.ayu.realty.member.model.request.MemberSaveReq;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/members")
+@RequiredArgsConstructor
+@Tag(name = "Member", description = "회원 관련 API")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Operation(
+            summary = "회원가입",
+            description = "사용자를 등록(이메일, 비밀번호, 닉네임)")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "회원가입 성공"),
+            @ApiResponse(responseCode = "409", description = "이메일 중복")
+    })
+    @PostMapping("/signup")
+    public ResponseEntity<ApiRes<Void>> signup(@RequestBody MemberSaveReq request){
+        boolean isSuccess = memberService.signup(request);
+        if (isSuccess) {
+            return ResponseEntity.ok(ApiRes.success(MemberSuccessCode.LOGIN_SUCCESS, null));
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(ApiRes.fail(ErrorCode.DUPLICATE_EMAIL));
+        }
+    }
+
+}
---
+++ src/main/java/com/ayu/realty/member/MemberService.java
@@ -0,0 +1,36 @@
+package com.ayu.realty.member;
+
+import com.ayu.realty.member.model.Member;
+import com.ayu.realty.member.model.Role;
+import com.ayu.realty.member.model.request.MemberSaveReq;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public boolean signup(MemberSaveReq request) {
+
+        // 이메일 중복 체크 로직
+        boolean isEmailExists = memberRepository.existsByEmail(request.getEmail());
+
+        if (isEmailExists) {
+            return false;
+        }
+
+        Member entity = Member.toEntity(
+                request.getEmail(),
+                request.getNickname(),
+                passwordEncoder.encode(request.getPassword()),// 비밀번호는 암호화하지 않고 저장
+                Role.ADMIN // 기본값으로 ADMIN 권한 부여
+        );
+
+        memberRepository.save(entity);
+        return true;
+    }
+}
