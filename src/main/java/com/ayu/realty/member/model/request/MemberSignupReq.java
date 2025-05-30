881ccabd52540bae3563a93d766465cb22670815
goodjunseon <pzs2001926@gmail.com>
2025-05-30 15:33:20 +0900

    feat: 회원가입 요청 DTO에 유효성 검증 어노테이션 추가

---
+++ src/main/java/com/ayu/realty/member/model/request/MemberSignupReq.java
@@ -0,0 +1,28 @@
+package com.ayu.realty.member.model.request;
+
+import com.ayu.realty.member.model.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MemberSignupReq {
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
+    private String password;
+
+    @NotBlank(message = "닉네임은 필수입니다.")
+    private String nickname;
+
+    @NotNull(message = "역할 정보가 필요합니다.")
+    private Role role;
+}
