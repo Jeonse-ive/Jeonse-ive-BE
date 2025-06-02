29a486c5e061490d34dcaaf313b6479badba7b73
goodjunseon <pzs2001926@gmail.com>
2025-06-03 03:47:11 +0900

    Rename: DTO명 변경

--- src/main/java/com/ayu/realty/member/model/response/MemberPrivateRes.java
+++ src/main/java/com/ayu/realty/member/model/response/MemberPrivateRes.java
@@ -6,7 +6,7 @@ import lombok.Data;
 
 @Data
 @Builder
-public class MemberAdminRes {
+public class MemberPrivateRes {
     private String email;
     private String password;
     private String nickname;
